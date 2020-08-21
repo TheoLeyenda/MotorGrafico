0dfd8803265fa942468d84fa2010406b188141e5
theixgamer <theofritaku@gmail.com>
2020-08-21 16:48:24 -0300

    Windows show

---
+++ Game/Game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
+#include "GameBase.h"
+class Game: GameBase
+{
+public:
+	Game();
+	~Game();
+	void Play();
+	bool isPlaying;
+};
+
+#endif
---
+++ Motor/Src/GameBase.h
@@ -0,0 +1,12 @@
+#ifndef GAMEBASE_H
+#define GAMEBASE_H
+
+#include "PrivateClass/Export.h"
+class ENGINE_API GameBase {
+
+public:
+	GameBase();
+	~GameBase();
+	int Init();
+};
+#endif
