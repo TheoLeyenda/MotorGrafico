7abb6a3654caefb75f4e1c90a96c1a5082b6e846
theixgamer <theofritaku@gmail.com>
2020-08-21 11:01:36 -0300

    configuracion nueva

--- Motor/main.cpp
+++ Motor/main.cpp
@@ -1,38 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "glew.h"
 #include "GLFW/glfw3.h"
 
 int main(void)
 {
-	GLFWwindow* window;
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Queremos OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Para hacer feliz a MacOS ; Aunque no debería ser necesaria
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //No queremos el viejo OpenGL 
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	//Crear una ventana y su contexto OpenGL
+	GLFWwindow* window; // (En el código que viene aqui, está variable es global)
+	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Falla al abrir una ventana GLFW. Si usted tiene una GPU Intel, está no es compatible con 3.3. Intente con la versión 2.1 de los tutoriales.\n");
 		glfwTerminate();
 		return -1;
 	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
+	glfwMakeContextCurrent(window); // Inicializar GLEW
+	glewExperimental = true; // Se necesita en el perfil de base.
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Falló al inicializar GLEW\n");
+		return -1;
 	}
-
-	glfwTerminate();
-	return 0;
 }
