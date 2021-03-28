d83bbc28f11c8f5393e80be26eb985564db1b6a2
FacundoPonce <mrfacu125@gmail.com>
2021-03-28 17:52:32 -0300

    Pyramid model updated.

--- Motor/Src/model.h
+++ Motor/Src/model.h
@@ -10,7 +10,7 @@ ENGINE_API enum TypeModel
 	Pyramid
 };
 
-const int indicesPyramid = 12;
+const int indicesPyramid = 24;
 const int indicesCube = 20;
 
 class ENGINE_API Model3D : public Entity
@@ -23,12 +23,13 @@ private:
 	  -0.0f, -0.5f,    0.0f,  1.0f,	  0.0f,	  0.0f,	   1.0f,
 
 	};
-	float vertexPyramid[28] = {
+	float vertexPyramid[35] = {
 		//	x		y		z		r		g		b		A
 		  -1.0f,  -1.0f,   0.0f,  1.0f,	  0.0f,	  0.0f,	   1.0f,
 		   0.0f,  -1.0f,   1.0f,  0.0f,	  1.0f,	  0.0f,	   1.0f,
 		   1.0f,  -1.0f,   0.0f,  0.0f,	  0.0f,	  1.0f,	   1.0f,
 		   0.0f,   1.0f,   0.0f,  1.0f,	  0.0f,	  0.0f,	   1.0f,
+		   0.0f,  -1.0f,  -1.0f,  0.0f,	  1.0f,	  0.0f,	   1.0f,
 	};
 	unsigned int indexCube[8] = {
 		
@@ -36,8 +37,10 @@ private:
 	unsigned int indexPyramid[indicesPyramid] = {
 		0,3,1,
 		1,3,2,
-		2,3,0,
-		0,1,2
+		0,3,4,
+		4,3,2,
+		0,1,2,
+		2,4,0
 	};
 	TypeModel _type;
 	unsigned int _ibo;
